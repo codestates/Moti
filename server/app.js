07d1670fabe5048a0538fec90e5f10fff24cb9ac
woobin0075 <woobin0075@gmail.com>
2021-08-25 09:52:46 +0900

    Add file structure

--- server/app.js
+++ server/app.js
@@ -1,7 +1,24 @@
 const express = require('express');
-
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
 const app = express();
-const port = 7000;
+const port = 80;
+const controllers = require('./controllers');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(
+    cors({
+      origin: true,
+      credentials: true,
+      methods: ['GET', 'POST', 'OPTIONS', 'PUT', 'PATCH', 'DELETE']
+    })
+  );
+
+app.use(cookieParser());
+
+app.post('/login', controllers.signin);
 
 app.get('/',(req,res)=>{
     res.status(201).send('hello world');
