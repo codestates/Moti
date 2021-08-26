0dc49309d42a9a56ba0547c8b5d2cb76443c3a32
woobin0075 <woobin0075@gmail.com>
2021-08-27 08:03:46 +0900

    fix models/index.js

--- server/controllers/post/allpost.js
+++ server/controllers/post/allpost.js
@@ -1,7 +1,7 @@
 const { Post, emotion, Post_emotion } = require('../../models');
 const { isAuthorized } = require('../tokenFunctions');
 
-module.exports = (req, res) => {
+module.exports = async (req, res) => {
         const authorization = req.headers['authorization'];
         if(!authorization){
             res.status(400).json({message : '유효한 회원이 아닙니다.'})
@@ -12,16 +12,23 @@ module.exports = (req, res) => {
             const num = userId.id;
 
             Post.findAll({
-               attributes : ['content', 'picture'],
-               include : [
-                   {
-                       model : Post_emotion,
-                       required : true
-                   }
-               ]
+                attributes : ['content', 'picture', 'createdAt'],
+                include : [{
+                    model: Post_emotion,
+                    required: true,
+                    include : [{
+                        model: emotion,
+                        required: true,
+                        attributes: ['emotion']
+                    }]
+                }],
+                where : {
+                    user_Id:  num
+                }
             })
             .then((result) => {
-                console.log(result);
+               // console.log(result[0].dataValues);
+                console.log(result[0].dataValues.Post_emotions[0].dataValues.emotion.dataValues.emotion); //이렇게 해야 emotion이 나옴
             })
         }
     
--- server/models/index.js
+++ server/models/index.js
@@ -36,16 +36,27 @@ db.Sequelize = Sequelize;
 
 const { Post, emotion, user, Post_emotion} = sequelize.models;
 
-user.hasMany(Post);
-Post.belongsTo(user);
+user.hasMany(Post, {
+  foreignKey: 'user_Id'
+}); //예네 2개를 주석처리하니깐 unknown column error가 안 난다. 근데 어떻게 처리해야 할지 모르겠다. 대신 업로드가 안됨
+Post.belongsTo(user, {
+  foreignKey: 'user_Id'
+}); 
 
-Post.hasMany(Post_emotion);
-Post_emotion.belongsTo(Post);
-Post.belongsToMany(emotion, { through: 'Post_emotion', targetKey:'id', foreignKey: 'Post_Id'});
+Post.hasMany(Post_emotion, {
+  foreignKey: 'Post_Id'
+});
+Post_emotion.belongsTo(Post, {
+  foreignKey: 'Post_Id'
+});
+Post.belongsToMany(emotion, { through: 'Post_emotion', foreignKey: 'Post_Id'});
 
-emotion.hasMany(Post_emotion);
-Post_emotion.belongsTo(emotion);
-emotion.belongsToMany(Post, {through: 'Post_emotion', targetKey:'id', foreignKey: 'emotion_Id'});
-
+emotion.hasMany(Post_emotion, {
+  foreignKey: 'emotion_Id'
+});
+Post_emotion.belongsTo(emotion, {
+  foreignKey: 'emotion_Id'
+});
+emotion.belongsToMany(Post, {through: 'Post_emotion', foreignKey: 'emotion_Id'});
 
 module.exports = db;
