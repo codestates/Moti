fa4b188855ef75aedb60d5bc8e3fe5e4007c8176
wktaylorla <wktaylorla@gmail.com>
2021-08-27 17:41:02 +0900

    modify login, signup and feat dropdown

--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -1,9 +1,30 @@
-import  React  from "react";
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import moti from '../assets/moti.svg';
 import search from '../assets/search.svg';
 
+import Modal from './Modal/Modal'
+
 function Header() {
+    const [dropdownVisible, setDropdownVisible] = useState(false);
+    const [modalState, setModalState] = useState('none');
+    // modal state는 none, profile, password로 분기.
+
+    const dropdownHandler = () => {
+        console.log(dropdownVisible)
+        setDropdownVisible(!dropdownVisible)
+    }
+
+    const modalHandler = (key) => (e) => {
+        console.log(key)
+        setModalState(key);
+    }
+
+    const logoutHandler = ()=>{
+        console.log('logout')
+        //서버에 로그아웃 요청, 개인정보 수정(로그아웃 상태로)
+    }
+
     return (
        <div className="header"> 
          <div className="header__container">
@@ -25,11 +46,31 @@ function Header() {
                           <img src={""} alt="profile" className="header__container__menu__item__profilebox__profile" />
                         </Link>
                     </li>
-                    <li className="header__container__menu__item">
-                       <img src="https://img.icons8.com/material/50/000000/616B7C/settings--v5.png" className="header__container__menu__item__setting"/>
+                    <li className="header__container__menu__item header__container__menu__item__setting-box">
+                        <button 
+                            className='header__container__menu__item__setting-box__btn'
+                            onClick={dropdownHandler}
+                        >
+                            <img src="https://img.icons8.com/material/50/000000/616B7C/settings--v5.png" 
+                                className="header__container__menu__item__setting"/>
+                        </button>
+                        <div className={dropdownVisible? 'header__container__menu__item__setting-box__dropdown' : 'header__container__menu__item__setting-box__dropdown hide'}>
+                            <ul>
+                                <li className="header__container__menu__item__setting-box__dropdown__list" onClick={modalHandler('profile')}>
+                                    프로필 수정
+                                </li>
+                                <li className="header__container__menu__item__setting-box__dropdown__list" onClick={modalHandler('password')}>
+                                    비밀번호 변경
+                                </li>
+                                <li className="header__container__menu__item__setting-box__dropdown__list" onClick={logoutHandler}>
+                                    로그아웃
+                                </li>
+                            </ul>
+                        </div>
                     </li>
                 </ul>
           </div>
+          {modalState!=='none'? <Modal modalState={modalState} modalHandler={modalHandler}/> : null}
        </div>
     )
 
