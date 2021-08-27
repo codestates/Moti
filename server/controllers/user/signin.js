61a26042501dc5a8fed0692c13887444ec01aa71
woobin0075 <woobin0075@gmail.com>
2021-08-27 22:29:04 +0900

    fix signin.js

--- server/controllers/user/signin.js
+++ server/controllers/user/signin.js
@@ -5,8 +5,9 @@ const { generateAccessToken, generateRefreshToken} = require('../tokenFunctions'
 module.exports = async (req, res) => {
     try {
         //const userInfo = await user.findOne({attributes: ['email', 'password', 'profile'], where: {email: req.body.email, password: req.body.password}});
-        const userInfo = await user.findOne({attributes: ['id', 'email', 'username', 'password'], where: {email: req.body.email}});
+        const userInfo = await user.findOne({attributes: ['id', 'email', 'username', 'password', 'profile'], where: {email: req.body.email}});
         const advice = await RandomAdvice.findOne({where : {id : req.body.adviceID}});
+        const userProfile = userInfo.dataValues.profile;
 
         if(!userInfo){
             return res.status(404).json({message : 'user Not found'});
@@ -17,6 +18,7 @@ module.exports = async (req, res) => {
                 return res.status(404).json({message : 'user Not found'});
             }else{
                 delete userInfo.dataValues.password //추가해봄.
+                delete userInfo.dataValues.profile
                 const access_token = generateAccessToken(userInfo.dataValues);
                 const refresh_token = generateRefreshToken(userInfo.dataValues);
 
@@ -24,25 +26,12 @@ module.exports = async (req, res) => {
                 res.json({data : {
                     accessToken : access_token, 
                     username : userInfo.dataValues.username,
-                    profile : userInfo.dataValues.profile,
+                    profile : userProfile,
                     RandomAdvice : advice.dataValues.advice
                 }, message : 'login ok'});
             }
         })
-        // if(!userInfo){
-        //     res.status(404).json({message : 'user Not found'});
-        // }else{
-        //     const access_token = generateAccessToken(userInfo.dataValues);
-        //     const refresh_token = generateRefreshToken();
-
-        //     res.cookie('RefreshToken', refresh_token, {httpOnly: true, sameSite: 'none', secure: true});
-        //     res.json({data : {
-        //         accessToken : access_token, 
-        //         username : userInfo.dataValues.username,
-        //         profile : userInfo.dataValues.profile,
-        //         RandomAdvice : advice.dataValues.advice
-        //     }, message : 'login ok'});
-        // }
+      
     } catch (error) {
         console.log(error);
     }
