697448d39a8a842997691e38dc7ccaad25f691aa
nala723 <enda11@naver.com>
2021-08-29 17:11:01 +0900

    re/upload

--- client/src/Pages/Mypage.js
+++ client/src/Pages/Mypage.js
@@ -5,22 +5,21 @@ import Footer from '../Components/Footer';
 import Post from '../Components/Post';
 import '../Components/mypage.css';
 
-function Mypage({loginHandler, userInfo, accessTokenHandler,logoutHandler}) {
+function Mypage({loginHandler, userInfo}) {
     // 구조분해해서 가져갈 부분만 가져가도
-    const { isLogin, accessToken, advice, username, profile} = userInfo
-    
-
+    // const { id, accessToken, username, profile, RandomAdvice} = userInfo
+    console.log(userInfo);
     return (
        <div className="mypage"> 
-        <Header loginHandler={loginHandler} userInfo={userInfo} logoutHandler={logoutHandler}/>
+        <Header loginHandler={loginHandler} userInfo={userInfo}/>
             <div className="mypage__container">
-             <Sidebar userInfo={userInfo} />
-             <Post isLogin={isLogin} accessToken={accessToken} accessTokenHandler={accessTokenHandler}/> 
+             <Sidebar userInfo={userInfo}/>
+             <Post userInfo={userInfo}/> 
             </div>
         <Footer />
        </div>
     )
 
 }
-// username={username} profile={profile}
+
 export default Mypage
