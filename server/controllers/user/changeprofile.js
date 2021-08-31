eb5510bea29396353174e8f61642d0b473de7633
yun sungmin <smy07207@gmail.com>
2021-08-31 12:39:13 +0900

    depexp

--- server/controllers/user/changeprofile.js
+++ server/controllers/user/changeprofile.js
@@ -8,26 +8,31 @@ module.exports = async (req,res) =>{
     console.log(req);
     console.log(username);
     const accesstoken = req.headers.authorization.split(' ')[1]; 
+    
     if(isAuthorized(accesstoken) === 'jwt expired'){
         token = remakeToken(req);
         res.set('accessToken', token); //헤더 설정
     }
 
     const data = jwt.verify(accesstoken, process.env.ACCESS_SECRET);
+    let user = await model.user.findOne({where : {email: data.email}});
+
     
-    if(req.body.picture === ''){//유저가 기본 이미지로 바꾸고 싶어함
+    if(req.body.picture === 'default_profile'){//유저가 기본 이미지로 바꾸고 싶어함
         image = fs.readFileSync(
             __basedir + "/resources/assets/tmp/bros_blank.jpg"
         )
     }
-    else if(req.file === undefined){ //유저가 이미지 수정을 원치 않음
-        image = null
+    else if(req.body.picture === ''){ //유저가 이미지 수정을 원치 않음
+        image = user.dataValues.profile;
     }
     else{
         image = fs.readFileSync(req.file.path);
     }
 
-    
+    if(username === ''){
+        username = user.dataValues.usernema;
+    }
     await model.user.update({ profile: image, username: username }, {
         where: {
           email: data.email
