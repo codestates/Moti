e56d1a7eada4ac00870df6f0d952a3d895c3fb5c
yun sungmin <smy07207@gmail.com>
2021-08-31 13:56:43 +0900

    depexp

--- server/controllers/user/oauthgit.js
+++ server/controllers/user/oauthgit.js
@@ -3,6 +3,7 @@ const model = require('../../models');
 const clientID = process.env.GITHUB_CLIENT_ID;
 const clientSecret = process.env.GITHUB_CLIENT_SECRET;
 const axios = require('axios');
+const { generateAccessToken, generateRefreshToken} = require('../tokenFunctions');
 
 module.exports = async (req,res)=>{
 
@@ -10,6 +11,7 @@ module.exports = async (req,res)=>{
     console.log(clientID);
     console.log(clientSecret);
     const advice = await model.RandomAdvice.findOne({where : {id : 1}}); 
+    
     //나중에 명언 dummy 넣고 랜덤 난수로찾기. client에서 받지말고 그냥 여기서 난수 생성
 
     axios({
@@ -43,25 +45,14 @@ module.exports = async (req,res)=>{
           })
 
         }
-        // let blob_image;
-        // fetch(gitInfo.data.avatar_url)
-        // .then(res => res.blob()) // Gets the response and returns it as a blob
-        // .then(blob => {
-        //   // Here's where you get access to the blob
-        //   // And you can use it for whatever you want
-        //   // Like calling ref().put(blob)
+        
+        const userInfo = await user.findOne({attributes: ['id', 'email', 'username'], where: {username: db_username}});
+        const access_token = generateAccessToken(userInfo.dataValues);
+        const refresh_token = generateRefreshToken(userInfo.dataValues);
+        res.cookie('RefreshToken', refresh_token, {httpOnly: true, sameSite: 'none', secure: true});
 
-        //   // Here, I use it to make an image appear on the page
-        //   // let objectURL = URL.createObjectURL(blob);
-        //   // let myImage = new Image();
-        //   // myImage.src = objectURL;
-        //   // document.getElementById('myImg').appendChild(myImage)
-        //   blob_image = blob;
-        // });
-
-        // res.status(200).json({ accessToken: accessToken })
         res.json({data : {
-          accessToken : accessToken, 
+          accessToken : access_token, 
           username : gitInfo.data.login,
           profile : gitInfo.data.avatar_url,
           RandomAdvice : advice.dataValues.advice,
